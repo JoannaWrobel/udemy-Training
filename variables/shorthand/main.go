fe8d5373e2f2f8a7437c673ab02f0e4e782c57c0
JoannaWrobel <joanna.izabela.wrobel@gmail.com>
2017-01-08 16:01:52 +0100

    training variables

---
+++ variables/shorthand/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main ()  {
+
+	a := "późno"
+	b := 10
+	c := 4.30
+	d := false
+
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+
+}

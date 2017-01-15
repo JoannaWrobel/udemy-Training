4860337961b32b031d1363a16d6fa9f122c3135d
JoannaWrobel <joanna.izabela.wrobel@gmail.com>
2017-01-15 18:31:33 +0100

    new elements

---
+++ constants/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const p string = "Death&taxes"
+const q  = 42
+const z bool= true
+
+func main()  {
+
+
+	fmt.Println("p -", p)
+	fmt.Println("q -", q)
+	fmt.Println("z -", z)
+	
+}
---
+++ constants/multiple/main.go
@@ -0,0 +1,14 @@
+package main
+
+import ("fmt")
+
+const (
+	PI = 3.14
+	Language = "golang"
+)
+
+func main()  {
+	fmt.Println("PI =", PI)
+	fmt.Println("language -", Language)
+
+}
